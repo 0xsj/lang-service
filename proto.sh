a8acb027c7957da2ce9f20e3a3f8f843397bb5ea
0xsj <sjtommylee@gmail.com>
2024-07-29 14:35:01 -0700

    build scripts, proto stubs

---
+++ proto.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Define arrays for Go services and the gateway
+goServices=("auth" "user")
+gateway=("auth" "user" "notification" "identity")
+
+# Cleanup function to remove old proto files and create a new directory
+cleanup() {
+  folderPath="$1/proto"
+  echo "Cleaning up $folderPath"
+  if [ -d "$folderPath" ]; then
+    rm -r "$folderPath"
+  fi
+  mkdir -p "$folderPath"
+}
+
+# Generate Go files from .proto files
+generateGo() {
+    path="$1/proto"
+    protoFile="$2.proto"
+    echo "Generating Go files for $protoFile in $path"
+    protoc --go_out="$path" --go-grpc_out="$path" --proto_path=proto "$protoFile"
+}
+
+# Generate Go files for each service
+for service in "${goServices[@]}"; do
+    echo "Processing service: $service"
+    cleanup "$service"
+    generateGo "$service" "$service"
+done
+
+# Generate Go files for the gateway
+echo "Processing conduit"
+cleanup "conduit"
+for file in "${gateway[@]}"; do
+    generateGo "conduit" "$file"
+done
